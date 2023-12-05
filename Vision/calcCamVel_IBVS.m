6d0ed03860927307306ddaae071636c5334496b3
Rouillermax <maxime.rouiller98@gmail.com>
2023-12-05 17:57:25 -0500

    Fini la partie asservissement visuel

--- Vision/calcCamVel_IBVS.m
+++ Vision/calcCamVel_IBVS.m
@@ -3,8 +3,29 @@ function twist = calcCamVel_IBVS(p,pd,Z,f)
 % p est une matrice 2 x n contenant les coordonnées image des n points
 % saillant
 % le resutat twist est [omega,v] pour la caméra 
+L = [];
+for k=1:length(p)
 
-twist = 0;  % à compléter
+    L1 = interactionMat(p(1,k),p(2,k),Z(k),f);
+    L = [L;
+        L1];
+end
+L_pinv = pinv(L);
+
+x = pd(1,:)-p(1,:);
+y = pd(2,:)-p(2,:);
+e0 = [pd(1,1)-p(1,1);
+    pd(2,1)-p(2,1)];
+e = [];
+
+for i=1:length(x)
+    e = [e;
+        x(i);
+        y(i)];
+end
+
+Kp = 15*eye(8);
+twist = L_pinv*Kp*e;  % à compléter
 
 end
 
--- Vision/interactionMat.m
+++ Vision/interactionMat.m
@@ -1,7 +1,8 @@
 function L = interactionMat(x,y,Z,f)
 %Renvoie la matrice d'interaction 2x6 pour un point saillant
 
-L = 0; % à compléter
+L = [x*y/f -(x^2+f^2)/f y  -f/Z 0 x/Z; 
+    (f^2+y^2)/f -x*y/f -x 0 -f/Z y/Z];
 
 end
 
